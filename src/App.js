25e0a81730718eedd4eef6e96b2c95093bc427bf
Aarnav Singh <aarnavsingh@Aarnavs-MacBook-Air.local>
2024-07-19 17:58:11 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -2,28 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import HeaderWithCarousel from './components/HeaderWithCarousel';
 import Footer from './components/Footer';
-{/*}
-import HomePage from './pages/HomePage';
-import StudioPage from './pages/StudioPage';
-import ServicesPage from './pages/ServicesPage';
-import ProjectsPage from './pages/ProjectsPage';
-import ContactPage from './pages/ContactPage';
-*/}
 
 function App() {
   return (
     <Router>
       <HeaderWithCarousel />
       <Footer/>
-      {/*
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/studio" element={<StudioPage />} />
-        <Route path="/services" element={<ServicesPage />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
-      */}
     </Router>
   );
 }
