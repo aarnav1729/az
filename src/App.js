a748623ee5001131f4524ba770fd4412e5c589b8
Aarnav Singh <aarnavsingh@Aarnavs-MacBook-Air.local>
2024-07-19 17:56:41 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import HeaderWithCarousel from './components/HeaderWithCarousel';
 import Footer from './components/Footer';
 {/*}
